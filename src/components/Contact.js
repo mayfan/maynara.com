09276c8b9188fe5f6f83492de9319ed41c5045a1
Ben Furber <ben@fu.rber.io>
2019-11-09 20:45:44 +0000

    ✨ Add Contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => (
+  <div class="contact">
+    <p>
+      Email: <a href="mailto:hello@maynara.com">hello@maynara.com</a>
+    </p>
+    <p>
+      <a href="https://maynara.com/linkedin">maynara.com/linkedin</a>
+    </p>
+    <p>
+      Instagram: <a href="https://instagram.com/">@empodereduasmulheres</a>
+    </p>
+  </div>
+);
+
+export default Contact;

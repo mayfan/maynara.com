524b2dfb5a8c132f996e7efa86fe9e7714ce92b9
Ben Furber <ben@fu.rber.io>
2019-11-15 21:28:56 +0000

    ✨ Add Rows component

---
+++ src/components/Rows.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
+
+export const Rows = ({ rows }) => {
+  return (
+    <div>
+      {rows.map(({ backgroundColor, column }) => (
+        <section className="section" style={{ backgroundColor }}>
+          <div className="container content" key={column}>
+            <div className="columns">
+              {column.map(({ content }) => (
+                <div className="column" key={content}>
+                  <ReactMarkdown source={content} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      ))}
+    </div>
+  );
+};
+
+Rows.propTypes = {
+  rows: PropTypes.array
+};

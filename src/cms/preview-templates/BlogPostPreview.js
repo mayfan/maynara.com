bf4f6c68114c740d1b9247605fd47e5c2063c640
Ben Furber <ben@fu.rber.io>
2019-11-16 15:24:01 +0000

    🔧 Fix blog preview

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,21 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BlogPostTemplate } from "../../templates/blog-post";
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor("body")}
-    description={entry.getIn(["data", "description"])}
-    tags={entry.getIn(["data", "tags"])}
-    title={entry.getIn(["data", "title"])}
-    rows={entry.getIn(["data", "rows"])}
-  />
-);
+const BlogPostPreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(["data"]).toJS();
+
+  if (data) {
+    return (
+      <BlogPostTemplate
+        content={data.body || ""}
+        description={data.description || ""}
+        title={data.title}
+        row={data.row || {}}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func
   }),
-  widgetFor: PropTypes.func
+  getAsset: PropTypes.func
 };
 
 export default BlogPostPreview;

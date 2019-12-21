efadb206a6a937d1da3cc2da58ffab7feb546d97
Ben Furber <ben@fu.rber.io>
2019-12-21 11:13:12 +0000

    ♻️ Change home contact link text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,13 @@ import React from "react";
 const Contact = () => (
   <div className="contact has-text-right">
     <p>
-      <a href="mailto:hello@maynara.com">hello@maynara.com</a>
+      <a href="mailto:hello@maynara.com">E-mail</a>
     </p>
     <p>
       <a href="https://www.linkedin.com/in/maynara-fanucci/">Linkedin</a>
     </p>
     <p>
-      <a href="https://instagram.com/">@empodereduasmulheres</a>
+      <a href="https://instagram.com/empodereduasmulheres">Instagram</a>
     </p>
   </div>
 );

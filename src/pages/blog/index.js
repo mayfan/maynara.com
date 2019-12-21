b737b6243c6090401b34c8889aa1d818bc94d94f
Ben Furber <ben@fu.rber.io>
2019-12-21 12:08:35 +0000

    ✨ Add green header to other pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,18 +7,25 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="full-width-image-container margin-top-0">
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: "0.5rem 0 0 #E86A92, -0.5rem 0 0 #E86A92",
-              backgroundColor: "#E86A92",
-              color: "white"
-            }}
-          >
-            Projects
-          </h1>
-        </div>
+        <section
+          className="section section--gradient"
+          style={{
+            backgroundColor: "#194239"
+          }}
+        >
+          <div className="container">
+            <div className="columns">
+              <div className="column is-9">
+                <div className="content header">
+                  <h1 className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
+                    My Projects
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <section className="section">
           <div className="container">
             <div className="content">

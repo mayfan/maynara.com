e4639cb9f26d2fdfb7488f1a2fd78416c7c14d15
Ben Furber <ben@fu.rber.io>
2019-12-21 12:17:56 +0000

    ✨ Add Header component for pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,31 +1,14 @@
 import React from "react";
 
-import Layout from "../../components/Layout";
 import BlogRoll from "../../components/BlogRoll";
+import Header from "../../components/Header";
+import Layout from "../../components/Layout";
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <section
-          className="section section--gradient"
-          style={{
-            backgroundColor: "#194239"
-          }}
-        >
-          <div className="container">
-            <div className="columns">
-              <div className="column is-9">
-                <div className="content header">
-                  <h1 className="is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
-                    My Projects
-                  </h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <Header title={"My Projects"} />
         <section className="section">
           <div className="container">
             <div className="content">

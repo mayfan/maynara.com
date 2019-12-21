d02220945c0f7e32ec9b446e4d618fda1c6f673f
Ben Furber <ben@fu.rber.io>
2019-12-21 10:46:13 +0000

    ✨ Add direct linkedin link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Contact = () => (
-  <div className="contact">
+  <div className="contact has-text-right">
     <p>
       <a href="mailto:hello@maynara.com">hello@maynara.com</a>
     </p>
     <p>
-      <a href="https://maynara.com/linkedin">maynara.com/linkedin</a>
+      <a href="https://www.linkedin.com/in/maynara-fanucci/">Linkedin</a>
     </p>
     <p>
       <a href="https://instagram.com/">@empodereduasmulheres</a>
